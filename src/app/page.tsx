0fdc217216e565afe4a60edf97e6b35c182cbfed
Iqbal Djamil <iqbaldjamil16@gmail.com>
2026-01-29 09:43:02 +0000

    hapus (_for element h1_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,11 +28,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-background p-8">
       <div className="w-full max-w-[16rem]">
-        <header className="mb-8 text-center">
-          <h1 className="font-headline text-3xl tracking-tight text-primary sm:text-5xl whitespace-nowrap [text-shadow:0_4px_0_hsl(var(--accent)/0.5)]">
-            PKH Mateng
-          </h1>
-        </header>
         <div className="grid grid-cols-2 gap-4">
           {menuItems.map((item: any, index) => (
             <MenuItem
