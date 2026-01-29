89837ef3dff8ab0a9e695db83a33410d8f9ca92b
Iqbal Djamil <iqbaldjamil16@gmail.com>
2026-01-29 08:53:44 +0000

    ubah bentuk font seperti gravity (_for element h1_)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Inter', 'sans-serif'],
-        headline: ['Cinzel', 'serif'],
+        headline: ['Bungee Shade', 'cursive'],
         code: ['monospace'],
       },
       colors: {
