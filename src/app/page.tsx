2e12a01ed92ac23f71b346c344df787846534511
Iqbal Djamil <iqbaldjamil16@gmail.com>
2026-01-29 08:58:32 +0000

    ubah ukurannya sedikit (_for element div_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ const menuItems = menuItemsData.map(item => {
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-background p-8">
-      <div className="w-full max-w-xs">
+      <div className="w-full max-w-sm">
         <header className="mb-8 text-center">
           <h1 className="font-headline text-3xl tracking-tight text-primary sm:text-5xl whitespace-nowrap [text-shadow:0_4px_0_hsl(var(--accent)/0.5)]">
             PKH Mateng
